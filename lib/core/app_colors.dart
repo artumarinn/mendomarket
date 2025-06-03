6302f462a340bde498ed88a68f1a0c139171669b
artumarinn <artumarin.09@gmail.com>
2025-06-03 19:51:48 -0300

    colors first page

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,5 +1,14 @@
 import 'dart:ui';
 
 class AppColors {
-  static const primaryColor = Color(0xFF6200EE);
+
+  // primary colors 
+  static const primaryColor = Color(0xFF4A7C59);
+  static const secondaryColor = Color(0xFFE9C46A);
+
+  // secondary colors
+  static const inputBackground = Color(0xFFF7F1DC);
+
+  // background colors
+  static const backgroundColor = Color(0xFFffffff);
 }
