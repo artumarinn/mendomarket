eb9017bcc3d5f3b550c2e93a6eab24d8e014390a
artumarinn <artumarin.09@gmail.com>
2025-06-27 21:08:16 -0300

    fix icon app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.mendomarket"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
